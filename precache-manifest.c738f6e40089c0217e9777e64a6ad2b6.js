96fcc13f70de0f65b79e5f9d8ecffc209cc262b7
Sionut0122 <sionut0122@yahoo.com>
2019-06-26 23:57:35 +0100

    Updates

---
+++ precache-manifest.c738f6e40089c0217e9777e64a6ad2b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "47dca03313f68244a953afd8f785eb34",
+    "url": "/PhotographyPortfolio/static/media/trevor_brown_gray.47dca033.jpg"
+  },
+  {
+    "revision": "0df819b809f5c6de726eeec236f1e875",
+    "url": "/PhotographyPortfolio/static/media/trevor_brown.0df819b8.jpg"
+  },
+  {
+    "revision": "a735d33c987127b9469193a016b51c73",
+    "url": "/PhotographyPortfolio/static/media/photographer-1149781_1920.a735d33c.jpg"
+  },
+  {
+    "revision": "8348f3bb3c03f99d2df830e7fd5830ba",
+    "url": "/PhotographyPortfolio/static/media/logo.8348f3bb.jpg"
+  },
+  {
+    "revision": "cc0e83dc2a54ea98914e",
+    "url": "/PhotographyPortfolio/static/js/runtime~main.795d9659.js"
+  },
+  {
+    "revision": "484b899517cc5bdd3761",
+    "url": "/PhotographyPortfolio/static/js/main.8097c97e.chunk.js"
+  },
+  {
+    "revision": "c7aaedc7f35abd92bb96",
+    "url": "/PhotographyPortfolio/static/js/2.a80d41ac.chunk.js"
+  },
+  {
+    "revision": "484b899517cc5bdd3761",
+    "url": "/PhotographyPortfolio/static/css/main.54950d04.chunk.css"
+  },
+  {
+    "revision": "048bb7e32815d80327557b535ef84941",
+    "url": "/PhotographyPortfolio/index.html"
+  }
+];
